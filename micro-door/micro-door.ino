c0719b8dacec0d2a6703e3b6c6b234b4a9f23969
kalmas <kylealmas@gmail.com>
2014-09-04 22:19:50 -0400

    comments galore

--- micro-door/micro-door.ino
+++ micro-door/micro-door.ino
@@ -1,32 +1,70 @@
 #include <SoftwareSerial.h>
 
+/*
+ * Lock control, on equals open
+ */
 const int lockPin = 2;
+
+/*
+ * RFID sensor serial input
+ */
 const int RFIDRX = 7;
+
+/*
+ * RFID sensor serial output
+ */
 const int RFIDTX = 8;
+
+/*
+ * RFID sensor reset
+ */
 const int RFIDResetPin = 9;
 
+/*
+ * We're using the "Software Serial" library to connect to RFID
+ * reader because we need the default Serial to be library to 
+ * read and write on USB channel
+ *
+ * Init software serial using RFID in/out
+ */
 SoftwareSerial mySerial(RFIDRX, RFIDTX); 
 
 void setup()
 {
-  // Start USB serial
+  /**
+   * Start serial for USB com
+   */
   Serial.begin(9600);
-  // Start RFID serial
+  
+  /**
+   * Start serial for RFID sensor com
+   */
   mySerial.begin(9600);
   
+  /**
+   * Init pin for sending "open sesame" signal
+   */
   pinMode(lockPin, OUTPUT);
   
+  /**
+   * Reset RFID sensor
+   */
   pinMode(RFIDResetPin, OUTPUT);
   digitalWrite(RFIDResetPin, HIGH);
 }
  
 void loop()
 {
-  // Pass RFID input along to USB
+  /*
+   * Pass RFID tag down the USB
+   */
   if (mySerial.available()) {
     Serial.write(mySerial.read());
   }
   
+  /**
+   * Control lock
+   */
   if (Serial.available()) {
     int incomingByte = Serial.read();
  
