833e5f6460154781783a62b7a760e4a0a12985b0
kalmas <kylealmas@gmail.com>
2014-09-21 15:35:31 -0400

    door opens!

--- node-door/routes/index.js
+++ node-door/routes/index.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+  router = express.Router(),
+  serialport = require('serialport'),
+  SerialPort = serialport.SerialPort,
+  serial = new SerialPort('/dev/tty.usbmodem1411', {
+    baudrate: 9600,
+    parser: serialport.parsers.readline("\n")
+  });
+
+var close = function() {
+  serial.write(new Buffer([0x00]));
+};
+
+var open = function() {
+  serial.write(new Buffer([0x01]));
+};
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Who Dat?' });
+});
+
+router.post('/open', function(req, res) {
+  res.render('open', { title: 'Its Open!'});
+  open();
+  setTimeout(close, 2000);
 });
 
 module.exports = router;
