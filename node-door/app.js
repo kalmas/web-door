da1b01b47c4e4299b6f20172b00cdb0f98e3a632
kalmas <kylealmas@gmail.com>
2014-11-06 23:48:43 -0500

    ui cleanup

--- node-door/app.js
+++ node-door/app.js
@@ -34,11 +34,7 @@ app.use(function(req, res, next) {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {},
-        title: 'error'
-    });
+    res.send();
 });
 
 var port = 80;
---
+++ node-door/public/js/main.js
@@ -0,0 +1,30 @@
+var red = function(){
+  $('body').css('background-color', '#FF4040');
+};
+
+var green = function(){
+	console.log('nice1');
+  $('body').css('background-color', '#00DD2F');
+};
+
+var open = function(){
+  $.ajax({
+  	type: "POST",
+  	url: '/open',
+  	success: function(){
+  	  isOpen = true;
+  	  green();
+  	  setTimeout(function(){red(); isOpen = false}, 2000);
+  	}
+  });
+};
+
+var isOpen = false;
+
+$(document).ready(function() {
+  $(document).click(function() {
+  	if(!isOpen){
+  	  open();
+  	}
+  });
+});
